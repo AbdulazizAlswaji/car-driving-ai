e62da19ae86aec3bd8c75e4ebf8562fb25823b60
Abdulaziz Alswaji <abdulaziz.alswaji@gmail.com>
2022-05-31 13:14:23 +0300

    finlize

--- sensor.js
+++ sensor.js
@@ -14,10 +14,13 @@ class Sensor {
         this.readings = [];
         for (let i = 0; i < this.rays.length; i++) {
             this.readings.push(
-                this.#getReading(this.rays[i], roadBorders, traffic)
+                this.#getReading(
+                    this.rays[i],
+                    roadBorders,
+                    traffic
+                )
             );
         }
-
     }
 
     #getReading(ray, roadBorders, traffic) {
@@ -25,29 +28,29 @@ class Sensor {
 
         for (let i = 0; i < roadBorders.length; i++) {
             const touch = getIntersection(
-                ray[0], ray[1],
+                ray[0],
+                ray[1],
                 roadBorders[i][0],
                 roadBorders[i][1]
-            )
-
+            );
             if (touch) {
                 touches.push(touch);
             }
         }
 
-        for(let i=0; i<traffic.length; i++) {
+        for (let i = 0; i < traffic.length; i++) {
             const poly = traffic[i].polygon;
-            for(let j=0; j<poly.length;j++) {
+            for (let j = 0; j < poly.length; j++) {
                 const value = getIntersection(
-                    ray[0], ray[1], poly[j],
-                    poly[(j+1)%poly.length]
+                    ray[0],
+                    ray[1],
+                    poly[j],
+                    poly[(j + 1) % poly.length]
                 );
-
-                if(value) {
+                if (value) {
                     touches.push(value);
                 }
             }
-
         }
 
         if (touches.length == 0) {
@@ -59,23 +62,22 @@ class Sensor {
         }
     }
 
-
     #castRays() {
         this.rays = [];
-
         for (let i = 0; i < this.rayCount; i++) {
             const rayAngle = lerp(
                 this.raySpread / 2,
-                - this.raySpread / 2,
+                -this.raySpread / 2,
                 this.rayCount == 1 ? 0.5 : i / (this.rayCount - 1)
             ) + this.car.angle;
 
             const start = { x: this.car.x, y: this.car.y };
             const end = {
-                x: this.car.x - Math.sin(rayAngle) * this.rayLength,
-                y: this.car.y - Math.cos(rayAngle) * this.rayLength,
-            }
-
+                x: this.car.x -
+                    Math.sin(rayAngle) * this.rayLength,
+                y: this.car.y -
+                    Math.cos(rayAngle) * this.rayLength
+            };
             this.rays.push([start, end]);
         }
     }
@@ -89,32 +91,29 @@ class Sensor {
 
             ctx.beginPath();
             ctx.lineWidth = 2;
-            ctx.strokeStyle = 'yellow';
+            ctx.strokeStyle = "yellow";
             ctx.moveTo(
                 this.rays[i][0].x,
-                this.rays[i][0].y,
+                this.rays[i][0].y
             );
             ctx.lineTo(
                 end.x,
-                end.y,
+                end.y
             );
             ctx.stroke();
 
-
             ctx.beginPath();
             ctx.lineWidth = 2;
-            ctx.strokeStyle = 'red';
+            ctx.strokeStyle = "black";
             ctx.moveTo(
                 this.rays[i][1].x,
-                this.rays[i][1].y,
+                this.rays[i][1].y
             );
             ctx.lineTo(
                 end.x,
-                end.y,
+                end.y
             );
             ctx.stroke();
         }
     }
 }
-
-
